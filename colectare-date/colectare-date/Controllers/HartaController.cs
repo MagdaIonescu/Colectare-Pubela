df965fa469fbbb85f9d99182ab1294a148e6d0b3
Magda <mionescu719@gmail.com>
2025-04-26 21:59:39 +0300

    Adaugare optimizare rute si afisare traseu cu ordinea punctelor pe harta
    Am implementat calculul rutei optimizate, afisarea traseelor neoptimizat/optimizat folosind Leaflet, marcarea punctelor cu ordinea de vizitare, adaugarea legendei pe harta, stilizarea paginilor (navbar, footer, formulare, tabele, harta).

--- colectare-date/colectare-date/Controllers/HartaController.cs
+++ colectare-date/colectare-date/Controllers/HartaController.cs
@@ -1,5 +1,8 @@
 ﻿using colectare_date.Data;
 using Microsoft.AspNetCore.Mvc;
+using ClosedXML.Excel;
+using colectare_date.ViewModels;
+using colectare_date.Models;
 
 namespace colectare_date.Controllers
 {
@@ -18,7 +21,148 @@ namespace colectare_date.Controllers
                 .Where(c => c.TimpColectare.Date == dataCautata)
                 .ToList();
 
-            return View(colectari);
+            var rutaNeoptimizata = Enumerable.Range(0, colectari.Count + 2).ToList();
+            var matrice = IncarcaMatrice();
+            var rutaOptimizata = CalculeazaRutaOptimizata(matrice);
+
+            var distantaTraseuNeoptimizat = CalculeazaDistanta(rutaNeoptimizata, matrice);
+            var distantaTraseuOptimizat = CalculeazaDistanta(rutaOptimizata, matrice);
+
+            var timpEstimativ = rutaOptimizata.Count * 1;
+
+            var rutaFaraStartStop = rutaOptimizata.Where(index => index > 0 && index <= colectari.Count).ToList();
+            var colectariOrdonate = rutaFaraStartStop
+                .Select(index => colectari[index - 1])
+                .ToList();
+
+            var model = new RutaOptimizataViewModel
+            {
+                Colectari = colectari,
+                ColectariOptimizate = rutaOptimizata.Where(index => index > 0 && index <= colectari.Count)
+                                        .Select(index => colectari[index - 1])
+                                        .ToList(),
+                DistantaTraseuNeoptimizat = distantaTraseuNeoptimizat,
+                DistantaTraseuOptimizat = distantaTraseuOptimizat,
+                DiferentaDistanta = distantaTraseuNeoptimizat - distantaTraseuOptimizat,
+                TimpEstimativ = timpEstimativ
+            };
+
+            return View(model);
         }
+
+        public int[,] IncarcaMatrice()
+        {
+            var caleFisier = "matrice.xlsx"; 
+            using var workbook = new XLWorkbook(caleFisier);
+            var ws = workbook.Worksheet("Matrix");
+            int nrLinii = ws.LastRowUsed().RowNumber();
+            int nrColoane = ws.LastColumnUsed().ColumnNumber();
+            var matrice = new int[nrLinii, nrColoane];
+
+            for (int i = 2; i <= nrLinii; i++) 
+            {
+                for (int j = 2; j <= nrColoane; j++) 
+                {
+                    var cellValue = ws.Cell(i, j).GetValue<double>();
+                    matrice[i - 2, j - 2] = (int)Math.Round(cellValue);
+                }
+            }
+
+            return matrice;
+        }
+
+        private int CalculeazaDistanta(List<int> ordine, int[,] matrice)
+        {
+            int distanta = 0;
+            for (int i = 0; i < ordine.Count - 1; i++)
+            {
+                int from = ordine[i];
+                int to = ordine[i + 1];
+                distanta += matrice[from, to];
+            }
+            return distanta;
+        }
+
+        public List<int> CalculeazaRutaOptimizata(int[,] matrice)
+        {
+            int n = matrice.GetLength(0);
+            bool[] vizitat = new bool[n];
+            List<int> ruta = new List<int>();
+            int current = 0;
+            ruta.Add(current);
+            vizitat[current] = true;
+
+            for (int i = 1; i < n; i++)
+            {
+                int next = -1;
+                int minDist = int.MaxValue;
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (!vizitat[j] && matrice[current, j] < minDist)
+                    {
+                        minDist = matrice[current, j];
+                        next = j;
+                    }
+                }
+
+                if (next != -1)
+                {
+                    ruta.Add(next);
+                    vizitat[next] = true;
+                    current = next;
+                }
+            }
+            return ruta;
+        }
+
+        public double CalculeazaDistantaTotala(List<Colectare> colectari, int[,] matrice)
+        {
+            double distanta = 0;
+            int current = 0; 
+
+            for (int i = 0; i < colectari.Count; i++)
+            {
+                int next = i + 1; 
+                distanta += matrice[current, next];
+                current = next;
+            }
+
+            int stopIndex = matrice.GetLength(0) - 1; 
+            distanta += matrice[current, stopIndex];
+
+            return distanta;
+        }
+
+        public IActionResult RutaOptimizata()
+        {
+            var colectari = context.Colectari
+                .Where(c => c.TimpColectare.Date == new DateTime(2024, 10, 15))
+                .ToList();
+
+            var matrice = IncarcaMatrice();
+            var ruta = CalculeazaRutaOptimizata(matrice);
+
+            var rutaFaraStartStop = ruta.Where(index => index > 0 && index <= colectari.Count).ToList();
+            var colectariOrdonate = rutaFaraStartStop
+                .Select(index => colectari[index - 1])
+                .ToList();
+
+            double distantaOptimizata = CalculeazaDistantaTotala(colectariOrdonate, matrice);
+            double distantaReala = CalculeazaDistantaTotala(colectari, matrice);
+
+            ViewBag.DistantaOptimizata = distantaOptimizata;
+            ViewBag.DistantaReala = distantaReala;
+            ViewBag.TimpColectari = colectariOrdonate.Count; 
+
+            var model = new RutaOptimizataViewModel
+            {
+                Colectari = colectari,
+                ColectariOptimizate = colectariOrdonate
+            };
+
+            return View(model);
+        }
+
     }
 }
